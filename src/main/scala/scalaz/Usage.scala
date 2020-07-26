3f21019f29fb2d89c748960c6fe7aba09e4fce5e
Niklas Leopold <niklas.leopold@verisure.com>
2020-07-26 13:49:28 +0200

    Kleisli

--- src/main/scala/scalaz/Usage.scala
+++ src/main/scala/scalaz/Usage.scala
@@ -8,7 +8,7 @@ import Validate._
 
 object Usage {
 
-  case class Person(name: String, age: Int, city: String, books: Set[String])
+  case class Person(name: String, age: Int, city: String, books: Set[String], salary: Int)
 
   def nameValidator(p: Person): Either[String, Person] =
     if (p.name.length > 0 && p.name.length < 21) Right(p) else Left("Name failed the validation")
@@ -33,12 +33,14 @@ object Usage {
       minValue("age", 0) >==>
       maxLength("city", 21) >==>
       minLengthString("city", 0) >==>
-      minLength[Set[String]]("books",0)
+      minLength[Set[String]]("books", 0) >==>
+      minValue("salary", 0)
 
   def main(args: Array[String]): Unit = {
     val books = Set("hepp")
-    val validPerson = Person("Niklas", 47, "Malmö", books)
-    val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", 47, "Malmö", books)
+    val validPerson = Person("Niklas", 47, "Malmö", books, 50000)
+    val validPerson2 = Person("Niklas", 47, "Malmö", books, 80000)
+    val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", 47, "Malmö", books, 500000)
 
     def personValidator1(p: Person): Either[String, Person] =
       nameValidator(p).flatMap(ageValidator).flatMap(cityValidator)
@@ -69,12 +71,58 @@ object Usage {
     // map
     println((personValidator3.map(_ => true)).run(validPerson))
 
-    println("************Val********************")
-    println(personValidator100(Person("NiklasNiklasNiklasNiklas", 46, "Malmö", books)))
-    println(personValidator100(Person("", 120, "Malmö", books)))
-    println(personValidator100(Person("Adam", 121, "Malmö", books)))
-    println(personValidator100(Person("Adam", 120, "Malmöööööööööööööööööööööööööööööö", books)))
+    // mapT && mapK
+    val optionValidator = personValidator3.mapT(e => if (e.isRight) Option(e.right) else None)
+    println(optionValidator(validPerson))
+
+    // flatMapK
+
+
+    println("************Validator********************")
+    println(personValidator100(Person("NiklasNiklasNiklasNiklas", 46, "Malmö", books, 10000)))
+    println(personValidator100(Person("", 120, "Malmö", books, 20000)))
+    println(personValidator100(Person("Adam", 121, "Malmö", books, 30000)))
+    println(personValidator100(Person("Adam", 120, "Malmöööööööööööööööööööööööööööööö", books, 40000)))
+    println(personValidator100(Person("Adam", 120, "Malmö", books, 0)))
     println(personValidator100(validPerson))
+
+    def logInput(lp: List[Person]): Either[Exception, List[Person]] = {
+      println(s"Will calculate the avg salary for ${lp.size} persons")
+      Right(lp)
+    }
+
+    def validatePersons(lp: List[Person]) : Either[Exception, List[Person]] =
+      personValidator100 traverse lp
+
+    final case class AvgException(message: String) extends Exception(message)
+
+    def calculateAvgSalary(lp: List[Person]): Either[Exception, Int] =
+      if(lp.size == 0) {
+        Left(AvgException("Avg on empty list"))
+      } else {
+        val (tot, sum) = lp.map(_.salary).foldLeft((0,0))((acc, s) => (acc._1 + 1, acc._2 + s))
+        Right(sum/tot)
+      }
+
+    def logSuccess(salary: Int): Either[Exception, Int] = {
+      println(s"The avarage salery is: $salary")
+      Right(salary)
+    }
+
+    val avgSalary =
+        Kleisli(validatePersons) >==>
+        calculateAvgSalary
+
+    val avgSalaryWithLogging =
+      Kleisli(logInput) >==>
+        avgSalary >==>
+        logSuccess
+
+    val result1: Either[Exception, Int] = avgSalaryWithLogging(List(validPerson, validPerson2))
+    println(result1)
+
+    val result2: Either[Exception, Int] = avgSalaryWithLogging(List(validPerson, validPerson2, invalidPerson))
+    println(result2)
 
   }
 
--- src/main/scala/scalaz/Validate.scala
+++ src/main/scala/scalaz/Validate.scala
@@ -3,14 +3,16 @@ package scalaz
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
 
+final case class ValidationException(message: String) extends Exception(message)
+
 object Validate {
-  type Validator[T] = T => Either[String, T]
+  type Validator[T] = T => Either[ValidationException, T]
 
   // https://stackoverflow.com/questions/6686992/scala-asinstanceof-with-parameterized-types
   def cast[A <: Any : Manifest](a: Any): A =
     manifest[A].runtimeClass.cast(a).asInstanceOf[A]
 
-  def getValue[I, O](obj: I, field: String)(implicit ct: ClassTag[I], tt: TypeTag[I], ct1: ClassTag[O], tt1: TypeTag[O]): Either[String, O] = {
+  def getValue[I, O](obj: I, field: String)(implicit ct: ClassTag[I], tt: TypeTag[I], ct1: ClassTag[O], tt1: TypeTag[O]): Either[ValidationException, O] = {
     try {
       val symbol = typeOf[I].member(TermName(field)).asMethod
       val m = runtimeMirror(obj.getClass.getClassLoader)
@@ -18,8 +20,8 @@ object Validate {
       val value: O = cast[O](im.reflectMethod(symbol).apply())
       Right(value)
     } catch {
-      case e: ClassCastException => Left(s"Wrong type on field $field:  $e")
-      case e: ScalaReflectionException => Left(s"Could not find a field called $field: $e")
+      case e: ClassCastException => Left(ValidationException(s"Wrong type on field $field:  $e"))
+      case e: ScalaReflectionException => Left(ValidationException(s"Could not find a field called $field: $e"))
     }
   }
 
@@ -28,7 +30,7 @@ object Validate {
     (t: T) => {
       for {
         value <- getValue[T, String](t, field)
-        result <- if (value.length < max) Right(t) else Left(s"Length on $field was ${value.length} but it must be less then $max")
+        result <- if (value.length < max) Right(t) else Left(new ValidationException(s"Length on $field was ${value.length} but it must be less then $max on $t"))
       } yield result
     }
   }
@@ -39,7 +41,7 @@ object Validate {
     (t: T) => {
       for {
         value <- getValue[T, String](t, field)
-        result <- if (value.length > min) Right(t) else Left(s"Length on $field was ${value.length} but it most be greater then $min")
+        result <- if (value.length > min) Right(t) else Left(ValidationException(s"Length on $field was ${value.length} but it most be greater then $min on $t"))
       } yield result
     }
   }
@@ -50,7 +52,7 @@ object Validate {
       (t: T) => {
         for {
           value <- getValue[T, FT](t, field)
-          result <- if (value.size > min) Right(t) else Left(s"Length on $field was ${value.size} but it most be greater then $min")
+          result <- if (value.size > min) Right(t) else Left(ValidationException(s"Length on $field was ${value.size} but it most be greater then $min on $t"))
         } yield result
       }
     }
@@ -59,17 +61,17 @@ object Validate {
   def minLength[FT <: Any {def size: Int}] = new MinLengthHelper[FT]
 
   def maxValue[T](field: String, max: Int)(implicit ct: ClassTag[T], tt: TypeTag[T], m: Manifest[T]): Validator[T] =
-    intValidation(field, max, v => v < max, v => s"Value on $field was $v but it must be less then $max")
+    intValidation(field, max, v => v < max, (t, v) => s"Value on $field was $v but it must be less then $max on $t")
 
   def minValue[T](field: String, min: Int)(implicit ct: ClassTag[T], tt: TypeTag[T], m: Manifest[T]): Validator[T] =
-    intValidation(field, min, value => value > min, v => s"Value on $field was $v but it must be greater then $min")
+    intValidation(field, min, value => value > min, (t, v) => s"Value on $field was $v but it must be greater then $min on $t")
 
-  private def intValidation[T](field: String, max: Int, op: Int => Boolean, errorMessage: Int => String)(implicit ct: ClassTag[T], tt: TypeTag[T], m: Manifest[T]): Validator[T] = {
+  private def intValidation[T](field: String, max: Int, op: Int => Boolean, errorMessage: (T, Int) => String)(implicit ct: ClassTag[T], tt: TypeTag[T], m: Manifest[T]): Validator[T] = {
     validateField(field)
     (t: T) => {
       for {
         value <- getValue[T, Integer](t, field)
-        result <- if (op(value)) Right(t) else Left(errorMessage(value))
+        result <- if (op(value)) Right(t) else Left(ValidationException(errorMessage(t, value)))
       } yield result
     }
   }
