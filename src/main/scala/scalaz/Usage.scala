6dff05ce7062a481d753d933f56e9290d24e5e65
Niklas Leopold <niklas.leopold@verisure.com>
2020-07-26 15:55:37 +0200

    Kleisli

--- src/main/scala/scalaz/Usage.scala
+++ src/main/scala/scalaz/Usage.scala
@@ -20,27 +20,17 @@ object Usage {
     if (p.city.length > 0 && p.city.length < 21) Right(p) else Left("City failed the validation")
 
 
-  def addOne(i: Int): Int = i + 1
-
-  def double(i: Int): Int = i * 2
-
-  def as(number: Int): String = Array.fill(number)("a").mkString
-
-  private val personValidator100 =
-    Kleisli(maxLength[Person]("name", 21)) >==>
-      minLengthString("name", 0) >==>
-      maxValue("age", 121) >==>
-      minValue("age", 0) >==>
-      maxLength("city", 21) >==>
-      minLengthString("city", 0) >==>
-      minLength[Set[String]]("books", 0) >==>
-      minValue("salary", 0)
-
   def main(args: Array[String]): Unit = {
+    basicUsage()
+
+    kleisliUsage()
+
+    validatorExample()
+  }
+
+  private def basicUsage() = {
     val books = Set("hepp")
     val validPerson = Person("Niklas", 47, "Malmö", books, 50000)
-    val validPerson2 = Person("Niklas", 47, "Malmö", books, 80000)
-    val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", 47, "Malmö", books, 500000)
 
     def personValidator1(p: Person): Either[String, Person] =
       nameValidator(p).flatMap(ageValidator).flatMap(cityValidator)
@@ -48,6 +38,12 @@ object Usage {
     println(personValidator1(validPerson))
 
     println("*************************")
+
+    def addOne(i: Int): Int = i + 1
+
+    def double(i: Int): Int = i * 2
+
+    def as(number: Int): String = Array.fill(number)("a").mkString
 
     val composedFunction1: Int => Int = addOne _ andThen double
     println(composedFunction1(20))
@@ -60,48 +56,63 @@ object Usage {
     // andThen
     val personValidator2 = Kleisli(nameValidator) >=> Kleisli(ageValidator) >=> Kleisli(cityValidator)
     println(personValidator2(validPerson))
+  }
 
-    val personValidator3 = Kleisli(nameValidator) >==> ageValidator >==> cityValidator
-    println(personValidator3(validPerson))
+  private def kleisliUsage() = {
+    val books = Set("hepp")
+    val validPerson = Person("Niklas", 47, "Malmö", books, 50000)
+    val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", 47, "Malmö", books, 500000)
+
+    val personValidator = Kleisli(nameValidator) >==> ageValidator >==> cityValidator
+    println(personValidator(validPerson))
 
     // traverse
-    println(personValidator3.traverse(List(validPerson, validPerson)))
-    println(personValidator3.traverse(List(validPerson, validPerson, invalidPerson)))
+    println(personValidator.traverse(List(validPerson, validPerson)))
+    println(personValidator.traverse(List(validPerson, validPerson, invalidPerson)))
 
     // map
-    println((personValidator3.map(_ => true)).run(validPerson))
+    println((personValidator.map(_ => true)).run(validPerson))
 
     // mapT && mapK
-    val optionValidator = personValidator3.mapT(e => if (e.isRight) Option(e.right) else None)
+    val optionValidator = personValidator.mapT((ep: Either[String, Person]) => if (ep.isRight) Option(ep.right) else None)
     println(optionValidator(validPerson))
 
     // flatMapK
+    println(personValidator.flatMapK((p: Person) => Right(p)))
+    // println(personValidator3.flatMapK(p => Option(p))) // will not compile
 
+    // flatMap
+    // personValidator3.flatMap(p: Person => )
+  }
 
-    println("************Validator********************")
-    println(personValidator100(Person("NiklasNiklasNiklasNiklas", 46, "Malmö", books, 10000)))
-    println(personValidator100(Person("", 120, "Malmö", books, 20000)))
-    println(personValidator100(Person("Adam", 121, "Malmö", books, 30000)))
-    println(personValidator100(Person("Adam", 120, "Malmöööööööööööööööööööööööööööööö", books, 40000)))
-    println(personValidator100(Person("Adam", 120, "Malmö", books, 0)))
-    println(personValidator100(validPerson))
+  private def validatorExample() = {
+    val personValidator100 =
+      Kleisli(maxLength[Person]("name", 21)) >==>
+        minLengthString("name", 0) >==>
+        maxValue("age", 121) >==>
+        minValue("age", 0) >==>
+        maxLength("city", 21) >==>
+        minLengthString("city", 0) >==>
+        minLength[Set[String]]("books", 0) >==>
+        minValue("salary", 0)
+
 
     def logInput(lp: List[Person]): Either[Exception, List[Person]] = {
       println(s"Will calculate the avg salary for ${lp.size} persons")
       Right(lp)
     }
 
-    def validatePersons(lp: List[Person]) : Either[Exception, List[Person]] =
+    def validatePersons(lp: List[Person]): Either[Exception, List[Person]] =
       personValidator100 traverse lp
 
     final case class AvgException(message: String) extends Exception(message)
 
     def calculateAvgSalary(lp: List[Person]): Either[Exception, Int] =
-      if(lp.size == 0) {
+      if (lp.size == 0) {
         Left(AvgException("Avg on empty list"))
       } else {
-        val (tot, sum) = lp.map(_.salary).foldLeft((0,0))((acc, s) => (acc._1 + 1, acc._2 + s))
-        Right(sum/tot)
+        val (tot, sum) = lp.map(_.salary).foldLeft((0, 0))((acc, s) => (acc._1 + 1, acc._2 + s))
+        Right(sum / tot)
       }
 
     def logSuccess(salary: Int): Either[Exception, Int] = {
@@ -110,13 +121,19 @@ object Usage {
     }
 
     val avgSalary =
-        Kleisli(validatePersons) >==>
+      Kleisli(validatePersons) >==>
         calculateAvgSalary
 
     val avgSalaryWithLogging =
       Kleisli(logInput) >==>
         avgSalary >==>
         logSuccess
+
+    val books = Set("hepp")
+    val validPerson = Person("Niklas", 47, "Malmö", books, 50000)
+    val validPerson2 = Person("Niklas", 47, "Malmö", books, 80000)
+    val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", 47, "Malmö", books, 500000)
+
 
     val result1: Either[Exception, Int] = avgSalaryWithLogging(List(validPerson, validPerson2))
     println(result1)
@@ -126,7 +143,7 @@ object Usage {
 
     val result3: Either[Exception, Int] = avgSalaryWithLogging(List())
     println(result3)
-
   }
+
 
 }
