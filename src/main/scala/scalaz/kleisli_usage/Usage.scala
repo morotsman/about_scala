a4f12098f50d0bc23d793ada84745e59c9956cd8
Niklas Leopold <niklas.leopold@verisure.com>
2020-07-29 10:14:57 +0200

    Kleisli

--- src/main/scala/scalaz/kleisli_usage/Usage.scala
+++ src/main/scala/scalaz/kleisli_usage/Usage.scala
@@ -1,8 +1,9 @@
 package scalaz.kleisli_usage
 
 import Validate._
-import scalaz.{Applicative, Kleisli}
-import scalaz.std.AllInstances._
+import scalaz._
+import Scalaz._
+import scalaz.Kleisli.kleisli
 
 import scala.util.Either.RightProjection
 
@@ -21,18 +22,32 @@ object Usage {
     val validPerson = Person("Niklas", "Malmö", books, 50000)
     val invalidPerson = Person("NiklasNiklasNiklasNiklasNiklasNiklasNiklas", "Malmö", books, 500000)
 
-    def personValidator1(p: Person): Either[String, Person] =
+    def personValidatorWithFlatMap(p: Person): Either[String, Person] =
       nameValidator(p).flatMap(salaryValidator).flatMap(cityValidator)
 
-    assert(Right(validPerson) == personValidator1(validPerson))
-    assert(Left("Name failed the validation") == personValidator1(invalidPerson))
+    assert(Right(validPerson) == personValidatorWithFlatMap(validPerson))
+    assert(Left("Name failed the validation") == personValidatorWithFlatMap(invalidPerson))
+
+    def personValidatorWithFlatMap2(p: Person): Either[String, Person] =
+      for {
+        _ <- nameValidator(p)
+        _ <- salaryValidator(p)
+        _ <- cityValidator(p)
+      } yield (p)
+
+    assert(Right(validPerson) == personValidatorWithFlatMap2(validPerson))
+    assert(Left("Name failed the validation") == personValidatorWithFlatMap2(invalidPerson))
 
     def addOne(i: Int): Int = i + 1
 
     def double(i: Int): Int = i * 2
 
-    val composedFunction1: Int => Int = addOne _ andThen double
-    assert(42 == composedFunction1(20))
+    def addOneAndDouble(i: Int): Int = double(addOne(20))
+
+    assert(42 == addOneAndDouble(20))
+
+    val composedFunction: Int => Int = addOne _ andThen double
+    assert(42 == composedFunction(20))
 
     def toAs(number: Int): String = Array.fill(number)("a").mkString
 
@@ -42,21 +57,65 @@ object Usage {
     // val composedFunction3 = (nameValidator _).andThen(salaryValidator) // will not compile, the types don't line up
 
     // andThen
-    val personValidator2 = Kleisli(nameValidator) >=> Kleisli(salaryValidator) >=> Kleisli(cityValidator)
-    assert(Right(validPerson) == personValidator2(validPerson))
-    assert(Left("Name failed the validation") == personValidator2(invalidPerson))
+    println("Kleisli")
+    val validator = Kleisli(nameValidator) andThen Kleisli(salaryValidator) andThen Kleisli(cityValidator)
+    println("Validator created")
+    assert(Right(validPerson) == validator(validPerson))
+    println("Validator has been executed")
+    assert(Left("Name failed the validation") == validator(invalidPerson))
+
+    val validator2 = Kleisli(nameValidator) >=> Kleisli(salaryValidator) >=> Kleisli(cityValidator)
+    assert(Right(validPerson) == validator2(validPerson))
+    assert(Left("Name failed the validation") == validator2(invalidPerson))
+
+    val validator3 = Kleisli(nameValidator) >==> salaryValidator >==> cityValidator
+    assert(Right(validPerson) == validator3(validPerson))
+    assert(Left("Name failed the validation") == validator3(invalidPerson))
+
+    val v: Kleisli[({type f[x] = Either[String, x]})#f, Person, Person] = Kleisli(nameValidator)
+    val result: Either[String, Person] = v(validPerson)
+
+
+    def personToPerson(p: Person): Person = p
+    // Kleisli(personToPerson) // will not compile
+
+    def personToPersons(p: Person): List[Person] = List(p)
+
+    Kleisli(personToPersons)
+
+    val b = implicitly[Bind[({type f[x] = Either[String, x]})#f]]
+
+    assert(Right(2) == b.bind(Right(1))(i => Right(i + 1)))
+
+    /*
+    def >=>[C](k: Kleisli[M, B, C])(implicit b: Bind[M]): Kleisli[M, A, C] =  kleisli((a: A) => b.bind(this(a))(k.run))
+     */
+    Kleisli(nameValidator) >=> Kleisli(salaryValidator)
+
+
+    val substitutedValidator = Kleisli((a: Person) => b.bind(Kleisli(nameValidator)(a))(Kleisli(salaryValidator).run))
+
+    assert(Right(validPerson) == substitutedValidator(validPerson))
+    assert(Left("Name failed the validation") == substitutedValidator(invalidPerson))
+
   }
 
   case class Person(name: String, city: String, books: Set[String], salary: Int)
 
-  def nameValidator(p: Person): Either[String, Person] =
+  def nameValidator(p: Person): Either[String, Person] = {
+    // println("nameValidator")
     if (p.name.length > 0 && p.name.length < 21) Right(p) else Left("Name failed the validation")
+  }
 
-  def cityValidator(p: Person): Either[String, Person] =
+  def cityValidator(p: Person): Either[String, Person] = {
+    // println("cityValidator")
     if (p.city.length > 0 && p.city.length < 21) Right(p) else Left("City failed the validation")
+  }
 
-  def salaryValidator(p: Person): Either[String, Person] =
+  def salaryValidator(p: Person): Either[String, Person] = {
+    // println("salaryValidator")
     if (p.salary > 0 && p.salary < 100000) Right(p) else Left("Salary failed the validation")
+  }
 
   private def kleisliUsage(): Unit = {
     val books = Set("Programing in Scala")
@@ -99,7 +158,7 @@ object Usage {
 
   private val personValidator =
     Validate[Person] >==>
-      (maxLengthString(20) on "name")  >==>
+      (maxLengthString(20) on "name") >==>
       (minLengthString(0) on "name") >==>
       (maxLengthString(20) on "city") >==>
       (minLengthString(0) on "city") >==>
