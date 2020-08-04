98da4fa7ace5f2d8eb0c6dd8d55ee31964ae1051
Niklas Leopold <niklas.leopold@verisure.com>
2020-08-04 15:36:32 +0200

    Applicative

--- src/main/scala/scalaz_experiments/validation/ValidateApplicative.scala
+++ src/main/scala/scalaz_experiments/validation/ValidateApplicative.scala
@@ -6,6 +6,7 @@ import Validator._
 import ValidatorApplicative._
 
 object ValidateApplicative {
+
   case class Person(name: String, age: Int)
 
   def validateName(name: String): Validated[String] =
@@ -17,37 +18,90 @@ object ValidateApplicative {
     else Invalid("Age failed the validation")
 
   def main(args: Array[String]): Unit = {
+    basicExamples()
+    validatorExample()
+  }
+
+  def basicExamples(): Unit = {
+    val someOne = 1.some
+    val someTwo = 2.some
+    val someThree = 3.some
+
+    def addOne(a: Int) = a + 1
+
+    val result1 = someOne.map(addOne)
+    assert(result1 == Some(2))
+
+    val result2 = someTwo <*> (addOne _).some
+    assert(result2 == Some(3))
+
+    def addTwo(a: Int, b: Int): Int = a + b
+
+
+    val result6 = someOne.map(a => someTwo.map(b => addTwo(a, b)))
+    assert(result6 == Some(Some(3)))
+
+    // if we're using Option as a monad we have access to flatMap...
+    val result5 = for {
+      a <- someOne
+      b <- someTwo
+    } yield addTwo(a, b)
+    assert(result5 == Some(3))
+
+    val result7 = ^(someOne, someTwo)(addTwo)
+    assert(result7 == Some(3))
+
+    val result8 = (someOne |@| someTwo) (addTwo)
+    assert(result8 == Some(3))
+
+    def addFour(a: Int, b: Int, c: Int, d: Int): Int = a + b + c + d
+
+    val result9 = ^^^(someOne, someTwo, someOne, someTwo)(addFour)
+    assert(result9 == Some(6))
+
+    val result10 = (someOne |@| someTwo |@| someOne |@| someTwo) (addFour)
+    assert(result10 == Some(6))
+
+    val result11 = someTwo +++ someTwo +++ someTwo
+    assert(result11 == Some(6))
+
+    implicit val times: Semigroup[Int] = new Semigroup[Int] {
+      override def append(f1: Int, f2: => Int): Int = f1 * f2
+    }
+
+    val result12 = Applicative[Option].plusA(someThree, someThree)(times)
+    assert(result12 == Some(9))
+
+    val result13 = someThree.+++(someTwo)(times) +++ someTwo
+    assert(result13 == Some(8))
+  }
+
+  def validatorExample(): Unit = {
     val validator = Applicative[Validated]
 
     val person1 = validator.apply2(validateName("NiklasNiklasNiklasNiklasNiklasNiklas"), validateAge(-1)) { (name, age) =>
       Person(name, age)
     }
 
-    println(person1)
+    assert(person1 == Invalid(Vector("Age failed the validation", "Name validation failed")))
 
     val person2: Validated[Person] = ^(validateName("NiklasNiklasNiklasNiklasNiklasNiklas"), validateAge(-1)) { (name, age) =>
       Person(name, age)
     }
 
-    println(person2)
-
+    assert(person2 == Invalid(Vector("Age failed the validation", "Name validation failed")))
 
     def createPerson(name: String, age: Int): Person =
       Person(name, age)
-
-    val validPerson = Person("Niklas", 30)
 
     val person3: Validated[Person] = validator.lift2(createPerson)(
       validateName("NiklasNiklasNiklasNiklasNiklasNiklas"),
       validateAge(7)
     )
+    assert(person3 == Invalid(Vector("Name validation failed")))
 
-    println(person3)
-
-    val result = (validateName("Niklas") |@| validateAge(30)) { (name: String, age: Int) => Person(name, age)}
-
-    println(result)
-
+    val result = (validateName("Niklas") |@| validateAge(30)) { (name: String, age: Int) => Person(name, age) }
+    assert(result == Valid(Person("Niklas", 30)))
   }
 
 }
