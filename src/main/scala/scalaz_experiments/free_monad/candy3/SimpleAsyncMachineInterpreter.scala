838b89ce54178fe400908d0d0392b5f7be13847b
Niklas Leopold <niklas.leopold@verisure.com>
2020-08-30 16:39:25 +0200

    Free monad

--- src/main/scala/scalaz_experiments/free_monad/candy3/SimpleAsyncMachineInterpreter.scala
+++ src/main/scala/scalaz_experiments/free_monad/candy3/SimpleAsyncMachineInterpreter.scala
@@ -15,24 +15,30 @@ object SimpleAsyncMachineInterpreter extends (MachineOp ~> ProgramResult) {
   private val machines = scala.collection.mutable.Map[Long, MachineState]()
 
   def apply[A](fa: MachineOp[A]) = fa match {
-    case UpdateState(id, f) => EitherT(Future.successful {
-      val newMachine = for {
-        oldM <- machines.get(id).toRight(new NoSuchElementException(s"Could not find a machine with id: $id"))
-        newM <- f(oldM)
-      } yield newM
-      newMachine.foreach(m => machines(id) = m)
-      newMachine
-    })
-    case CurrentState(id) => EitherT(Future.successful {
-      machines
-        .get(id)
-        .toRight(new NoSuchElementException(s"Could not find a machine with id: $id"))
-    })
-    case InitialState(machine) => EitherT(Future {
-      val newMachine = machine.copy(id = Some(id))
-      machines(id) = newMachine
-      id = id + 1
-      Right(newMachine): Either[Exception, MachineState]
-    })
+    case UpdateState(id, f) => EitherT(updateState(id, f))
+    case CurrentState(id) => EitherT(getCurrentState(id))
+    case InitialState(machine) => EitherT(setInitialState(machine))
+  }
+
+  def updateState(id: Long, f: MachineState => Either[Exception, MachineState]): Future[Either[Exception, MachineState]] = Future {
+    val newMachine = for {
+      oldM <- machines.get(id).toRight(new NoSuchElementException(s"Could not find a machine with id: $id"))
+      newM <- f(oldM)
+    } yield newM
+    newMachine.foreach(m => machines(id) = m)
+    newMachine
+  }
+
+  def getCurrentState(id: Long): Future[Either[Exception, MachineState]] = Future {
+    machines
+      .get(id)
+      .toRight(new NoSuchElementException(s"Could not find a machine with id: $id"))
+  }
+
+  def setInitialState(machine: MachineState): Future[Either[Exception, MachineState]] = Future {
+    val newMachine = machine.copy(id = Some(id))
+    machines(id) = newMachine
+    id = id + 1
+    Right(newMachine): Either[Exception, MachineState]
   }
 }
